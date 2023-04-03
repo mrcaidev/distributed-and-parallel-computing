92c4d237e5b23fbd80e9ffa579ac305e06ff4f8a
mrcaidev <mrcaidev@gmail.com>
2023-04-03 11:07:40 +0800

    fix: use long long type

--- src/sieve-of-eratosthenes/optimized-cache.cpp
+++ src/sieve-of-eratosthenes/optimized-cache.cpp
@@ -2,6 +2,20 @@
 #include <math.h>
 #include <stdio.h>
 
+#define LL long long
+
+LL convertCharToLongLong(char *str)
+{
+    LL result = 0;
+    int index = 0;
+    while (str[index] != '\0')
+    {
+        result = result * 10 + str[index] - '0';
+        index++;
+    }
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
     // 初始化 MPI 环境。
@@ -27,10 +41,10 @@ int main(int argc, char *argv[])
     }
 
     // 从命令行参数中解析出 n。
-    int n = atoi(argv[1]);
+    LL n = convertCharToLongLong(argv[1]);
 
     // 如果进程数过多，就退出。
-    if (2 + (n - 1) / processCount < (int)sqrt((double)n))
+    if (2 + (n - 1) / processCount < (LL)sqrt(n))
     {
         if (processId == 0)
         {
@@ -42,12 +56,12 @@ int main(int argc, char *argv[])
     }
 
     // 每个进程各自找出 [3, sqrt(n)] 的素数，用作后续筛选的基数。
-    int sqrtN = (int)sqrt((double)n);
-    int subLowerBound = 3;
-    int subUpperBound = sqrtN % 2 == 0 ? sqrtN - 1 : sqrtN;
+    LL sqrtN = (LL)sqrt(n);
+    LL subLowerBound = 3;
+    LL subUpperBound = sqrtN % 2 == 0 ? sqrtN - 1 : sqrtN;
 
     // 创建标记数组，用于标记 [3, sqrt(n)] 的每个奇数是否为合数。
-    int subSize = (subUpperBound - subLowerBound) / 2 + 1;
+    LL subSize = (subUpperBound - subLowerBound) / 2 + 1;
     bool *subCompositeFlags = (bool *)malloc(subSize);
 
     // 如果内存不足，就退出。
@@ -58,53 +72,49 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // 初始化标记数组全为 false。
-    for (int flagIndex = 0; flagIndex < subSize; flagIndex++)
-    {
-        subCompositeFlags[flagIndex] = false;
-    }
+    memset(subCompositeFlags, 0, subSize);
 
     // 从 3 为基数开始，检查每个奇数是否为基数的倍数。
-    for (int flagIndex = 0; flagIndex < subSize; flagIndex++)
+    for (LL flagIndex = 0; flagIndex < subSize; flagIndex++)
     {
         // 如果当前基数已被标记为合数，则跳过。
-        if (subCompositeFlags[flagIndex] == true)
+        if (subCompositeFlags[flagIndex])
         {
             continue;
         }
 
         // 找到第一个当前基数的倍数。
-        int base = flagIndex * 2 + 3;
-        int firstMultipleIndex = (base * base - subLowerBound) / 2;
+        LL base = flagIndex * 2 + 3;
+        LL firstMultipleIndex = (base * base - subLowerBound) / 2;
 
         // 把所有当前基数的倍数都标记为合数。
-        for (int multipleIndex = firstMultipleIndex; multipleIndex < subSize; multipleIndex += base)
+        for (LL multipleIndex = firstMultipleIndex; multipleIndex < subSize; multipleIndex += base)
         {
             subCompositeFlags[multipleIndex] = true;
         }
     };
 
     // 计算当前进程的负责范围的下界。
-    int lowerBound = 2 + processId * (n - 1) / processCount;
+    LL lowerBound = 2 + processId * (n - 1) / processCount;
     if (lowerBound % 2 == 0)
     {
         lowerBound++;
     }
 
     // 计算当前进程的负责范围的上界。
-    int upperBound = 1 + (processId + 1) * (n - 1) / processCount;
+    LL upperBound = 1 + (processId + 1) * (n - 1) / processCount;
     if (upperBound % 2 == 0)
     {
         upperBound--;
     }
 
     // 负责范围内的数字总数。
-    int totalSize = (upperBound - lowerBound) / 2 + 1;
+    LL totalSize = (upperBound - lowerBound) / 2 + 1;
 
     // 根据缓存大小，将负责范围划分为多个块。
-    int CACHE_SIZE = 4194304;
-    int blockSize = CACHE_SIZE / sizeof(int) / processCount;
-    int blockNum = totalSize / blockSize;
+    LL CACHE_SIZE = 4194304;
+    LL blockSize = CACHE_SIZE / sizeof(LL) / processCount;
+    LL blockNum = totalSize / blockSize;
     if (totalSize % blockSize != 0)
     {
         blockNum++;
@@ -122,20 +132,17 @@ int main(int argc, char *argv[])
     }
 
     // 创建计数器，用于记录负责范围内的素数个数。
-    int count = 0;
+    LL count = 0;
 
     // 依次处理每个块。
-    for (int blockIndex = 0; blockIndex < blockNum; blockIndex++)
+    for (LL blockIndex = 0; blockIndex < blockNum; blockIndex++)
     {
         // 重置标记数组全为 false。
-        for (int flagIndex = 0; flagIndex < blockSize; flagIndex++)
-        {
-            compositeFlags[flagIndex] = false;
-        }
+        memset(compositeFlags, 0, blockSize);
 
         // 计算当前块的下界和上界。
-        int blockLowerBound = lowerBound + blockIndex * blockSize * 2;
-        int blockUpperBound = blockLowerBound + blockSize * 2 - 1;
+        LL blockLowerBound = lowerBound + blockIndex * blockSize * 2;
+        LL blockUpperBound = blockLowerBound + blockSize * 2 - 1;
         if (blockUpperBound > upperBound)
         {
             blockUpperBound = upperBound;
@@ -144,10 +151,10 @@ int main(int argc, char *argv[])
         {
             blockUpperBound--;
         }
-        int realBlockSize = (blockUpperBound - blockLowerBound) / 2 + 1;
+        LL realBlockSize = (blockUpperBound - blockLowerBound) / 2 + 1;
 
         // 从 3 为基数开始，检查每个奇数是否为基数的倍数。
-        for (int flagIndex = 0; flagIndex < subSize; flagIndex++)
+        for (LL flagIndex = 0; flagIndex < subSize; flagIndex++)
         {
             // 如果当前基数已被标记为合数，则跳过。
             if (subCompositeFlags[flagIndex])
@@ -156,11 +163,11 @@ int main(int argc, char *argv[])
             }
 
             // 找到范围内第一个当前基数的倍数。
-            int base = flagIndex * 2 + 3;
-            int firstMultipleIndex = 0;
+            LL base = flagIndex * 2 + 3;
+            LL firstMultipleIndex = 0;
             if (base * base > blockLowerBound)
             {
-                firstMultipleIndex = (base * base - blockLowerBound) / 2;
+                firstMultipleIndex = ((LL)base * base - blockLowerBound) / 2;
             }
             else if (blockLowerBound % base == 0)
             {
@@ -177,14 +184,14 @@ int main(int argc, char *argv[])
             }
 
             // 把范围内所有当前基数的倍数都标记为合数。
-            for (int multipleIndex = firstMultipleIndex; multipleIndex < realBlockSize; multipleIndex += base)
+            for (LL multipleIndex = firstMultipleIndex; multipleIndex < realBlockSize; multipleIndex += base)
             {
                 compositeFlags[multipleIndex] = true;
             }
         };
 
         // 计算范围内的素数个数。
-        for (int flagIndex = 0; flagIndex < realBlockSize; flagIndex++)
+        for (LL flagIndex = 0; flagIndex < realBlockSize; flagIndex++)
         {
             if (!compositeFlags[flagIndex])
             {
@@ -194,7 +201,7 @@ int main(int argc, char *argv[])
     }
 
     // 进程间通信，计算总素数个数。
-    int globalCount = 0;
+    LL globalCount = 0;
     if (processCount > 1)
     {
         MPI_Reduce(&count, &globalCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -209,7 +216,7 @@ int main(int argc, char *argv[])
     // 进程 0 输出结果。
     if (processId == 0)
     {
-        printf("There are %d primes less than or equal to %d\n", globalCount, n);
+        printf("There are %lld primes less than or equal to %lld\n", globalCount, n);
         printf("SIEVE (%d) %10.6f\n", processCount, elapsedTime);
     }
 
